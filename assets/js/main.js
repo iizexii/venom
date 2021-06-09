cf6fb3af90e3abd9bad584651ae6299285603388
iizexii <iizexii@gmail.com>
2021-06-09 13:31:06 +0300

    index + adaptive

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// document.addEventListener('DOMContentLoaded', () => {
+// });
+$(document).ready(() => {
+	$("body #header").load("/header.html");
+	$("body #footer").load("/footer.html");
+
+	$(document).on('click', '.nav__mobile', function () {
+		let navBtn = $('.nav__mobile');
+		if (navBtn.hasClass('nav__mobile_active')) {
+			$('body').removeClass('fixed');
+			$('.nav__list').removeClass('nav__list_active');
+			$('.nav__mobile').removeClass('nav__mobile_active');
+		} else {
+			$('body').addClass('fixed');
+			$('.nav__list').addClass('nav__list_active');
+			$('.nav__mobile').addClass('nav__mobile_active');
+		}
+
+	})
+})
